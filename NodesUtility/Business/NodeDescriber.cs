ebe713dbabd64b7c9b3cd28417a24d1a645f7bad
Vishnu Aman <vishnu.aman@outlook.com>
2016-08-07 10:02:54 +1200

    Added NodeDescriberTests class and added nochildren test

---
+++ NodesUtility/Business/NodeDescriber.cs
@@ -0,0 +1,13 @@
+using NodesUtility.Interfaces;
+using NodesUtility.Modals;
+
+namespace NodesUtility.Business
+{
+    public class NodeDescriber : INodeDescriber
+    {
+        public string Describe(Node node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ NodesUtilityTests/NodeDescriberTests.cs
@@ -0,0 +1,22 @@
+﻿using NodesUtility.Business;
+using NodesUtility.Modals;
+using NUnit.Framework;
+
+namespace NodesUtilityTests
+{
+    [TestFixture]
+    public class NodeDescriberTests
+    {
+
+        [Test]
+        public void Describe_InputNoChildrenNode_OutputsSameDescription()
+        {
+            var expected = "new NoChildrenNode(\"root\")";
+
+            var nodeDescriber = new NodeDescriber();
+            var testInput = new NoChildrenNode("root");
+            var actual = nodeDescriber.Describe(testInput);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
