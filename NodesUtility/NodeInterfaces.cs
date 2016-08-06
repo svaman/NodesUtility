4fc830dff73277e93735668460e8bbab032a00bc
Vishnu Aman <vishnu.aman@outlook.com>
2016-08-06 19:04:08 +1200

    Initial commit with provided node classes

---
+++ NodesUtility/NodeInterfaces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodesUtility
+{
+    public interface INodeDescriber
+    {
+        string Describe(Node node);
+    }
+
+    public interface INodeTransformer
+    {
+        Node Transform(Node node);
+    }
+
+    public interface INodeWriter
+    {
+        Task WriteToFileAsync(Node node, string filePath);
+    }
+}
