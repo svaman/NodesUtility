06d9c023f18b44f6ce3863149cdccae96ce2ac2b
Vishnu Aman <vishnu.aman@outlook.com>
2016-08-08 00:55:33 +1200

    Refactored file

--- NodesUtility/Business/NodeTransformer.cs
+++ NodesUtility/Business/NodeTransformer.cs
@@ -11,24 +11,21 @@ namespace NodesUtility.Business
             {
                 return node;
             }
-            else
+            dynamic parent = node;
+            int numberOfChildren = parent.Children.Length;
+            switch (numberOfChildren)
             {
-                dynamic parent = node;
-                int numberOfChildren = parent.Children.Length;
-                switch (numberOfChildren)
-                {
-                    case 0:
-                        return new NoChildrenNode(node.Name);
-                    case 1:
-                        return new SingleChildNode(node.Name, 
-                                                Transform(parent.Children[0]));
-                    case 2:
-                        return new TwoChildrenNode(node.Name,
-                                                Transform(parent.Children[0]),
-                                                Transform(parent.Children[1]));
-                }
-                return node;
+                case 0:
+                    return new NoChildrenNode(node.Name);
+                case 1:
+                    return new SingleChildNode(node.Name, 
+                        Transform(parent.Children[0]));
+                case 2:
+                    return new TwoChildrenNode(node.Name,
+                        Transform(parent.Children[0]),
+                        Transform(parent.Children[1]));
             }
+            return node;
         }
     }
 }
