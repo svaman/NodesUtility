a0651935e1c05339e3af3e9d231e860ee91491f5
Vishnu Aman <vishnu.aman@outlook.com>
2016-08-08 00:50:01 +1200

    Renamed test names for better readability- missing files

---
+++ NodesUtility/Infrastructure/NinjectFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Ninject;
+
+namespace NodesUtility.Infrastructure
+{
+    public static class NinjectFactory
+    {
+        private static IKernel _kernel;
+
+        private static IKernel Kernel
+        {
+            get
+            {
+                if (_kernel != null) return _kernel;
+
+                _kernel = new StandardKernel();
+                _kernel.Load(Assembly.GetExecutingAssembly());
+                return _kernel;
+            }
+        }
+
+        public static T GetInstance<T>()
+        {
+            return Kernel.Get<T>();
+        }
+    }
+}
